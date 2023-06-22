830aa46f138c129bcde42277c4d53d6442b7bae2
Yomna147 <122878558+Yomna147@users.noreply.github.com>
2023-06-22 14:42:16 +0300

    Update mul.c

--- mul.c
+++ mul.c
@@ -1,32 +1,21 @@
 #include "monty.h"
-/**
- * f_mul - multiplies the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_mul(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
 
-	h = *head;
-	while (h)
+/**
+ * _mul -> Multiplies the top two elements of the stack.
+ * @stack: Stack.
+ * @line_number: Line Number.
+*/
+
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	int num;
+
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	aux = h->next->n * h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+	} num = (*stack)->next->n;
+	num *= (*stack)->n;
+	_pop(stack, line_number);
+	(*stack)->n = num;
 }
