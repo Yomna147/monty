de7f4a757d60f593c3b6eea8d768ba593fd76398
mohamed khaled <123021751+Kavloa@users.noreply.github.com>
2023-06-23 07:55:40 +0300

    Update func3.c

--- func3.c
+++ func3.c
@@ -87,7 +87,7 @@ void _mul(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: cn't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	} nm = (*stack)->next->n;
 	nm *= (*stack)->n;
