4b676f60c8dc4f0f23cb944e13df1cf143a2278d
mohamed khaled <123021751+Kavloa@users.noreply.github.com>
2023-06-23 07:57:46 +0300

    Update func3.c

--- func3.c
+++ func3.c
@@ -42,12 +42,12 @@ void pchar(stack_t **head, unsigned int counter)
 {
     if (*head == NULL)
     {
-        fprintf(stderr, "L%d: cn't pchar, stack mpty\n", counter);
+        fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
         exit(EXIT_FAILURE);
     }
     if ((*head)->n < 0 || (*head)->n > 127)
     {
-        fprintf(stderr, "L%d: cn't pchar, value out of rnge\n", counter);
+        fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
         exit(EXIT_FAILURE);
     } printf("%c\n", (*head)->n);
 }
